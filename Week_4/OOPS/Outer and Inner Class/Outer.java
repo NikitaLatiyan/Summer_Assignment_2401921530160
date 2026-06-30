7206d59b5d8341ebf95e31fc016857f40cb403bb
Nikita Chhaudhary <nikitalatiyan@gmail.com>
2026-06-30 20:46:37 +0530

    Add Outer and Inner class implementation

---
+++ Week_4/OOPS/Outer and Inner Class/Outer.java
@@ -0,0 +1,13 @@
+class Outer {
+
+    void display() {
+        System.out.println("Display method of Outer class");
+    }
+
+    class Inner {
+
+        void display() {
+            System.out.println("Display method of Inner class");
+        }
+    }
+}
